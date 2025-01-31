2e56f1c614845880f621362b559012197f484bd9
JacenL <jason@Jasons-MacBook-Pro.local>
2025-01-31 12:32:39 -0500

    fixed embeds not sending

--- cogs/UtilitiesCog.py
+++ cogs/UtilitiesCog.py
@@ -28,21 +28,16 @@ class UtilitiesCog(commands.Cog):
 
     @commands.command()
     async def coinflip(self, ctx):
-        embed1 = discord.Embed(title="Flipping...", color=discord.Color.blue())
-        message = await ctx.reply(embed=embed1)
+        embed = discord.Embed(title="Flipping...", color=discord.Color.blue())
+        message = await ctx.reply(embed=embed)
         await asyncio.sleep(2)
-        embed2 = discord.Embed(title=f"{random.choice(['Heads!', 'Tails!'])}", color=discord.Color.green())
-        await message.edit(embed=embed2)
-
-    @commands.command()
-    async def die(self, ctx):
-        await ctx.reply("dying...dead")
-        await self.bot.close()
+        embed = discord.Embed(title=f"{random.choice(['Heads!', 'Tails!'])}", color=discord.Color.green())
+        await message.edit(embed=embed)
 
     @commands.command()
     async def message(self, ctx, username, *, message):
         if not username or not message:
-            embed = discord.Embed(title=f"Error", description=f"Please provide a username.", color=discord.Color.red)
+            embed = discord.Embed(title=f"Error", description=f"Please provide a username.", color=discord.Color.red())
             await ctx.reply(embed=embed)
             return
         member = discord.utils.get(ctx.guild.members, name=username)
@@ -51,47 +46,47 @@ class UtilitiesCog(commands.Cog):
         if member:
             try:
                 await member.send(message)
-                embed = discord.Embed(title="Success", description=f"Sent message to {username}.", color=discord.Color.green)
+                embed = discord.Embed(title="Success", description=f"Sent message to {username}.", color=discord.Color.green())
                 await ctx.reply(embed=embed)
             except discord.errors.Forbidden:
-                embed = discord.Embed(title=f"Error", description=f"Could not send message to {username}. They may have DMs disabled.", color=discord.Color.red)
+                embed = discord.Embed(title=f"Error", description=f"Could not send message to {username}. They may have DMs disabled.", color=discord.Color.red())
                 await ctx.reply(embed=embed)
             return
-        embed = discord.Embed(title=f"Error", description=f"User {username} not found.", color=discord.Color.red)
+        embed = discord.Embed(title=f"Error", description=f"User {username} not found.", color=discord.Color.red())
         await ctx.reply(embed=embed)
     
     @commands.command()
     async def messageping(self, ctx, member:discord.Member, *, message):
         if not message:
-            embed = discord.Embed(title=f"Error", description=f"Please type in a message.", color=discord.Color.red)
+            embed = discord.Embed(title=f"Error", description=f"Please type in a message.", color=discord.Color.red())
             await ctx.reply(embed=embed)
             return
         try:
             await member.send(message)
-            embed = discord.Embed(title="Success", description=f"Sent message to {member.mention}.", color=discord.Color.green)
+            embed = discord.Embed(title="Success", description=f"Sent message to {member.mention}.", color=discord.Color.green())
             await ctx.reply(embed=embed)
         except discord.errors.Forbidden:
-            embed = discord.Embed(title=f"Error", description=f"Could not send message to {member.mention}. They may have DMs disabled.", color=discord.Color.red)
+            embed = discord.Embed(title=f"Error", description=f"Could not send message to {member.mention}. They may have DMs disabled.", color=discord.Color.red())
             await ctx.reply(embed=embed)
 
     @commands.command()
     async def day(self, ctx):
         url = 'https://www.mbhs.edu'
         try:
-            await ctx.send("Loading...1")
             response = requests.get(url)
             if response.status_code == 200:
-                await ctx.send("Loading...2")
-                result = re.search(r"\bTomorrow is an (?:ODD|EVEN) day\b", str(response.text))
-                await ctx.send("Loading...3")
-                print(result.group())
-                embed = discord.Embed(title="Day", description=f"{result.group()}", color=discord.Color.green)
-                await ctx.send("Loading...4")
+                result = re.search(r"\b(?:Today|Tomorrow) is an (?:ODD|EVEN) day\b", str(response.text))
+                embed = discord.Embed(title="Day", description=f"{result.group()}", color=discord.Color.green())
             else:
-                embed = discord.Embed(title="Error", description=f"Could not get data from {url}.", color=discord.Color.red)
+                embed = discord.Embed(title="Error", description=f"Could not get data from {url}.", color=discord.Color.red())
         except Exception:
-            embed = discord.Embed(title="Error", description=f"Something really bad happened.", color=discord.Color.red)
+            embed = discord.Embed(title="Error", description=f"Something really bad happened.", color=discord.Color.red())
         await ctx.reply(embed=embed)
+
+    @commands.command()
+    async def die(self, ctx):
+        await ctx.reply("dying...dead")
+        await self.bot.close()
 
 
 async def setup(bot):
